2592d99d344182c331563681425dc71e1d8aa198
Paul Cheney <spartandesign.biz@gmail.com>
2025-03-05 21:53:26 -0700

    remove final

--- final-project/scripts/mobile-nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerBtn = document.querySelector('.hamburger-btn');
-    const mobileNav = document.querySelector('.mobile-nav');
-
-    hamburgerBtn.addEventListener('click', function () {
-        mobileNav.style.display = mobileNav.style.display === 'flex' ? 'none' : 'flex';
-    });
-});
